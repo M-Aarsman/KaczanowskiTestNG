e91f9d4761d09a217da21719328e6da689444499
Magdalena_Aarsman <magdalena_aarsman@epam.com>
2018-03-27 19:12:01 +0200

    pass all tests

--- src/main/java/com/magda/kaczanowski/tests/Chapter3ExOne.java
+++ src/main/java/com/magda/kaczanowski/tests/Chapter3ExOne.java
@@ -5,6 +5,11 @@ import java.util.List;
 
 public class Chapter3ExOne {
     public static String reverse(String s) {
+
+        if(s == null) {
+            throw new IllegalArgumentException("string to reverse can not be null!");
+        }
+
         List<String> tempArray = new ArrayList<>(s.length());
         for (int i = 0; i < s.length(); i++) {
             tempArray.add(s.substring(i, i+1));
