99a76e76a146349204db7cb8dc655d14c55ceb85
Magdalena_Aarsman <magdalena_aarsman@epam.com>
2018-03-27 21:36:26 +0200

    refactor change to stream version

--- src/main/java/com/magda/kaczanowski/tests/Chapter3ExOne.java
+++ src/main/java/com/magda/kaczanowski/tests/Chapter3ExOne.java
@@ -7,7 +7,9 @@ class Chapter3ExOne {
             throw new IllegalArgumentException("string to reverse can not be null!");
         }
 
-        StringBuilder reversedString = new StringBuilder(s);
-        return reversedString.reverse().toString();
+        return s.chars().mapToObj(c -> (char)c).map(c -> Character.toString(c)).reduce("", (s1,s2) -> s2+s1);
+
+        //StringBuilder reversedString = new StringBuilder(s);
+        //return reversedString.reverse().toString();
     }
 }
