ffbf6e424483a565f9822598316f54e15ce2c56d
Magdalena_Aarsman <magdalena_aarsman@epam.com>
2018-03-28 19:37:04 +0200

    done with constructor

--- src/main/java/com/magda/kaczanowski/chapter4/FootballTeam.java
+++ src/main/java/com/magda/kaczanowski/chapter4/FootballTeam.java
@@ -4,6 +4,9 @@ class FootballTeam {
     private final int gamesWon;
 
     FootballTeam(int gamesWon) {
+        if(gamesWon < 0) {
+            throw new IllegalArgumentException("number of games won can not be negative");
+        }
         this.gamesWon = gamesWon;
     }
 
--- src/test/java/com/magda/kaczanowski/chapter4/FootballTeamTest.java
+++ src/test/java/com/magda/kaczanowski/chapter4/FootballTeamTest.java
@@ -1,15 +1,38 @@
 package com.magda.kaczanowski.chapter4;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
 
-@Test
 public class FootballTeamTest {
-    public void constructorShouldSetGamesWon() {
-        int gamesWon = 3;
-        FootballTeam team = new FootballTeam(gamesWon);
-        assertEquals(team.getGamesWon(), gamesWon, gamesWon + " were passed to constructor, but "
+
+    @DataProvider
+    private Object[][] numberOfGamesWon() {
+        return new Object[][] {
+                {0},
+                {1},
+                {2}
+        };
+    }
+
+    @DataProvider
+    private Object[][] illegalNumberOfGamesWon() {
+        return new Object[][]{
+                {-1},
+                {-10}
+        };
+    }
+
+    @Test(dataProvider = "numberOfGamesWon")
+    public void constructorShouldSetGamesWon(int numberOfGamesWon) {
+        FootballTeam team = new FootballTeam(numberOfGamesWon);
+        assertEquals(team.getGamesWon(), numberOfGamesWon, numberOfGamesWon + " were passed to constructor, but "
         + team.getGamesWon() + " were returned");
+    }
+
+    @Test(dataProvider = "illegalNumberOfGamesWon", expectedExceptions = IllegalArgumentException.class)
+    public void shouldThrowExceptionForIllegalGamesNumber(int illegalNumberOfGames) {
+        new FootballTeam(illegalNumberOfGames);
     }
 }
